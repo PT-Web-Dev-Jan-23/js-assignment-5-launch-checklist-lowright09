1967af3318cba72d6746efca46aa9de86942d83f
lowright09 <lowright09@gmail.com>
2023-03-18 13:39:35 -0500

    modify scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -69,13 +69,15 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
         });
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let randomPlanet = Math.floor(Math.random()*planets.length);
+    return planets[randomPlanet]
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
