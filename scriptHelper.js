12cbbcc80f02192a0501d990aa535ed98148140b
lowright09 <lowright09@gmail.com>
2023-03-18 16:30:21 -0500

    scripthelper.js modify6

--- scriptHelper.js
+++ scriptHelper.js
@@ -47,24 +47,24 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
             }
             
             if (fuelLevel < 10000 && cargoLevel <= 10000) {
-                fuel.innerHTML= `not enough fuel for the journey ` ;
+                fuel.innerHTML= "Fuel level too low for launch " ;
                 cargo.innerHTML = "Cargo mass low enough for launch";
-                launchStatus.innerHTML = `Shuttle not ready for take off `;
-                launchStatus.style.color = "rgb (199, 37,78)";
+                launchStatus.innerHTML = "Shuttle Not Ready for Launch ";
+                launchStatus.style.color = "rgb (199, 37, 78)";
             } else if (fuelLevel >= 10000 && cargoLevel >10000){
                 fuel.innerHTML = "Fuel level high enough for launch";
                 cargo.innerHTML = "Cargo mass too heavy for launch";
-                launchStatus.innerHTML = `Shuttle not ready for take off `;
-                launchStatus.style.color = "rgb (199, 37,78)";
+                launchStatus.innerHTML = "Shuttle Not Ready for Launch ";
+                launchStatus.style.color = "rgb (199, 37, 78)";
             } else if (fuelLevel < 10000 && cargoLevel > 10000) {
                 fuel.innerHTML = "Fuel level too low enough for launch";
                 cargo.innerHTML = "Cargo mass too heavy for launch";
-                launchStatus.innerHTML = `Shuttle not ready for take off `;
-                launchStatus.style.color = "rgb (199, 37,78)";
+                launchStatus.innerHTML = "Shuttle Not Ready for Launch ";
+                launchStatus.style.color = "rgb (199, 37, 78)";
             } else {
                 fuel.innerHTML = "Fuel level high enough for launch";
                 cargo.innerHTML = "Cargo mass low enough for launch";
-                launchStatus.innerHTML = `Shuttle is ready for take off `;
+                launchStatus.innerHTML = "Shuttle is Ready for Launch";
                 
             }
 
