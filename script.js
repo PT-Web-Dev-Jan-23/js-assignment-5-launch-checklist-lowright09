00c12acfac30edbc53f4de61dbecc397427ac2b6
lowright09 <lowright09@gmail.com>
2023-03-18 14:11:50 -0500

    modify script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-const { myFetch } = require("./scriptHelper");
+const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
@@ -13,6 +13,17 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+        let planet = pickPlanet(listedPlanets);
+        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image)
+    })
+        let list = document.getElementById("faultyItems");
+        list.style.visibility = "hidden";
+        let form = document.querySelector("form");
+       form.addEventListener("submit", function(event) {
+       event.preventDefault();
+       let pilot = document.querySelector("input[name=pilotName]").value;
+       let copilot = document.querySelector("input[name=copilotName]").value;
+       let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+       let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+       formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
 });
